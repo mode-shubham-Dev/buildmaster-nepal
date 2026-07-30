05c656e480afa0bb53b099eb75255a9355780d5f
mode-shubham-Dev <modedeveloper28@gmail.com>
2026-07-30 14:39:57 +0545

    Module 2: add API v1 versioning to all routes

--- api/routes/api.php
+++ api/routes/api.php
@@ -4,10 +4,12 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Api\Auth\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login']);
+Route::prefix('v1')->group(function () {
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/login', [AuthController::class, 'login']);
 
-Route::middleware('auth:sanctum')->group(function () {
-    Route::post('/logout', [AuthController::class, 'logout']);
-    Route::get('/me', [AuthController::class, 'me']);
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::post('/logout', [AuthController::class, 'logout']);
+        Route::get('/me', [AuthController::class, 'me']);
+    });
 });
